91621b64735a94275ea51c68fa376a60010a389f
rxqiang <rxq@sin.com>
2022-03-11 19:21:32 +0800

    fix

--- src/decorators/paginate.decorator.ts
+++ src/decorators/paginate.decorator.ts
@@ -4,13 +4,13 @@ export const Paginate = createParamDecorator(
   (_data: unknown, ctx: ExecutionContext) => {
     const { body } = ctx.switchToHttp().getRequest();
 
-    Reflect.has(body, 'limit')
-      ? Reflect.defineProperty(body, 'limit', { value: +body.limit })
-      : Reflect.defineProperty(body, 'limit', { value: 20 });
+    Reflect.defineProperty(body, 'limit', {
+      value: Reflect.has(body, 'limit') ? +body.limit : 20
+    });
 
-    Reflect.has(body, 'page')
-      ? Reflect.defineProperty(body, 'page', { value: +body.page })
-      : Reflect.defineProperty(body, 'page', { value: 1 });
+    Reflect.defineProperty(body, 'page', {
+      value: Reflect.has(body, 'page') ? +body.limit : 1
+    });
 
     Object.assign(body, { take: body.limit });
     const skip = (body.page - 1) * body.limit;
